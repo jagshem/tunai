1c244499dbce12e9f4156bb7cfa01c1e813e7dc8
Tunahan Dogan <piyealar@gmail.com>
2023-07-27 23:14:36 +0300

    Hover Card

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { Montserrat } from 'next/font/google'
+import { usePathname } from 'next/navigation'
 
 import { cn } from '@/lib/utils'
 import {
@@ -14,6 +15,15 @@ import {
   Code,
   Settings,
 } from 'lucide-react'
+import { CalendarDays } from 'lucide-react'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card'
+import { useState, useEffect } from 'react'
 
 const montserrat = Montserrat({ weight: '600', subsets: ['latin'] })
 
@@ -23,36 +33,48 @@ const routes = [
     icon: LayoutDashboard,
     href: '/dashboard',
     color: 'text-sky-500',
+    content:
+      'Dashboard sayfası, kullanıcının uygulamanın genel performansını ve istatistiklerini görüntüleyebileceği bir kontrol panelidir.',
   },
   {
     label: 'Conversation',
     icon: MessageSquare,
     href: '/conversation',
     color: 'text-violet-500',
+    content:
+      'Conversation sayfası, yapay zeka destekli sohbetler ve metin tabanlı iletişimler oluşturmak için kullanılabilir.',
   },
   {
     label: 'Image Generation',
     icon: ImageIcon,
     href: '/image',
     color: 'text-pink-700',
+    content:
+      'Image Generation sayfası, kullanıcının metin veya konsept bazlı girdilere dayalı olarak sanatsal veya gerçekçi görseller üretebileceği bir arayüzdür.',
   },
   {
     label: 'Video Generation',
     icon: VideoIcon,
     href: '/video',
     color: 'text-orange-700',
+    content:
+      'Video Generation sayfası, kullanıcının metin veya senaryo bazlı girdilere göre video içerikleri oluşturabileceği bir arayüzdür.',
   },
   {
     label: 'Music Generation',
     icon: Music,
     href: '/music',
     color: 'text-emerald-700',
+    content:
+      'Music Generation sayfası, kullanıcının melodi, ritim veya tür bazlı girdilere göre müzik veya ses parçaları oluşturabileceği bir arayüzdür.',
   },
   {
     label: 'Code Generation',
     icon: Code,
     href: '/code',
     color: 'text-green-700',
+    content:
+      'Code Generation sayfası, kullanıcının metin tabanlı girdilere dayalı olarak yazılım kodları veya algoritmalar üretebileceği bir arayüzdür.',
   },
   {
     label: 'Settings',
@@ -62,6 +84,16 @@ const routes = [
 ]
 
 const Sidebar = () => {
+  const pathname = usePathname()
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -73,20 +105,57 @@ const Sidebar = () => {
             Tunai
           </h1>
         </Link>
-        <div className="space-y-2">
+        <>
           {routes.map((route) => (
-            <Link
-              href={route.href}
-              key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
-            >
-              <div className="flex items-center flex-1">
-                <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
-                {route.label}
-              </div>
-            </Link>
+            <HoverCard key={route.href}>
+              <HoverCardTrigger>
+                <div className="mb-2.5">
+                  <Link
+                    href={route.href}
+                    key={route.href}
+                    className={cn(
+                      'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+                      pathname === route.href
+                        ? 'text-white bg-white/10'
+                        : 'text-zinc-400'
+                    )}
+                  >
+                    <div className="flex items-center flex-1">
+                      <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
+                      {route.label}
+                    </div>
+                  </Link>
+                </div>
+              </HoverCardTrigger>
+              {route.content && (
+                <HoverCardContent className="w-80 ml-3">
+                  <div className="flex justify-end space-x-4">
+                    <Avatar>
+                      <div className="flex items-center justify-center">
+                        <AvatarImage
+                          src="/logo.png"
+                          alt="@tunai"
+                          className="h-8 w-8 ml-[6px]"
+                        />
+                      </div>
+                      <AvatarFallback>VC</AvatarFallback>
+                    </Avatar>
+                    <div className="space-y-1">
+                      <h4 className="text-sm font-bold">@tunai</h4>
+                      <p className="text-sm font-semibold">{route.content}</p>
+                      <div className="flex items-center pt-2">
+                        <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{' '}
+                        <span className="text-xs text-muted-foreground">
+                          @Tunai Joined 07/27/2023
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </HoverCardContent>
+              )}
+            </HoverCard>
           ))}
-        </div>
+        </>
       </div>
     </div>
   )
